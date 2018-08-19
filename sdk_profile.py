da608a3b303035e6765979379f1ee1d2529fc469
Wang Jingtuan <wangjingtuan@qiyi.com>
2018-08-19 12:45:20 +0800

    Improvement: improve sdk_profile.py generate a plist file
    Signed-off-by: Wang Jingtuan <wangjingtuan@qiyi.com>

--- sdk_profile.py
+++ sdk_profile.py
@@ -1,42 +1,63 @@
 import subprocess
-import plistlib
 import time
+import plistlib
+
 
 class SDKProfile(object):
-	"""docstring for SDKProfile"""
-	def __init__(self, path):
-		super(SDKProfile, self).__init__()
-		self.path = path
+    """docstring for SDKProfile"""
 
-	def run_command(self, command):
-		dir_cmd = 'cd {directory}'.format(directory = self.path)
-		cmd = '{dir_cmd}; {cmd}'.format(dir_cmd = dir_cmd, cmd = command)
-		return subprocess.check_output(cmd, shell = True)
+    def __init__(self):
+        super(SDKProfile, self).__init__()
 
-	def git_checkout_all(self):
-		self.run_command('git checkout .')
+    def run_command(self, command):
+        return subprocess.check_output(command, shell=True)
 
-	def git_pull_rebase(self):
-		self.run_command('git pull --rebase')
+    def git_checkout_all(self):
+        self.run_command('git checkout .')
 
-	def git_commit_hash(self):
-		return self.run_command('git rev-parse HEAD').strip().decode('utf-8')
+    def git_pull_rebase(self):
+        self.run_command('git pull --rebase')
 
-	def git_commit_short_hash(self):
-		return self.run_command('git rev-parse --short HEAD').strip().decode('utf-8')
+    def git_commit_hash(self):
+        return self.run_command('git rev-parse HEAD').strip().decode('utf-8')
 
-	def git_branch(self):
-		return self.run_command('git rev-parse --abbrev-ref HEAD').strip().decode('utf-8')
+    def git_commit_short_hash(self):
+        return self.run_command('git rev-parse --short HEAD').strip().decode('utf-8')
+
+    def git_branch(self):
+        return self.run_command('git rev-parse --abbrev-ref HEAD').strip().decode('utf-8')
+
+    def build_time(self):
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+
+    def sdk_profile_info(self):
+        branch = '  branch: {branch} \n'.format(branch=sdk_profile.git_branch())
+        commit_id = '  commit_id: {commit_id} \n'.format(commit_id=sdk_profile.git_commit_hash())
+        build_time = '  build_time: {build_time} \n'.format(build_time=sdk_profile.build_time())
+
+        return "/* \n" + branch + commit_id + build_time + "*/ \n"
+
+    def sdk_profile_dict(self):
+
+        branch = '{branch}'.format(branch=sdk_profile.git_branch())
+        commit_id = '{commit_id}'.format(commit_id=sdk_profile.git_commit_hash())
+        build_time = '{build_time}'.format(build_time=sdk_profile.build_time())
+
+        plistDict = {
+            "branch": branch,
+            "commit_id": commit_id,
+            "build_time": build_time,
+        }
+
+        return plistDict
 
 
-sdkProfile = SDKProfile('/Users/wangjingtuan/Documents/workspace/qypay')
-sdkProfile.git_checkout_all()
-sdkProfile.git_pull_rebase()
+if __name__ == '__main__':
 
-plistDict = {
-		'commit_id' : sdkProfile.git_commit_short_hash(),
-		'branch' : sdkProfile.git_branch(),
-		'time' : time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
-	}
+    sdk_profile = SDKProfile()
+    sdk_profile.git_checkout_all()
+    sdk_profile.git_pull_rebase()
 
-plistlib.writePlist(plistDict, "/Users/wangjingtuan/Documents/workspace/qypay/sdkProfile.plist")
+    sdk_plist_path = './sdk_profile.plist'
+
+    plistlib.writePlist(sdk_profile.sdk_profile_dict(), sdk_plist_path)
